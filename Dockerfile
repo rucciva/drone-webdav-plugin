10a90f52c78b63351126bd4ea7d2eb5100bd846c
Rucciva <rucciva@gmail.com>
2020-06-02 13:51:51 +0700

    try

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,17 @@
 FROM appropriate/curl
 
+ARG HTTP_PROXY
 ARG URL=https://github.com/ncw/rclone/releases/download/v1.46/rclone-v1.46-linux-amd64.zip
 WORKDIR /tmp
-RUN curl -o rclone.zip -L $URL
-RUN unzip rclone.zip 
+RUN HTTP_PROXY=${HTTP_PROXY} curl -o rclone.zip -L $URL
+RUN unzip rclone.zip
 
-FROM alpine:3.9.2
+FROM alpine:3.11.6
 
-RUN apk add --no-cache ca-certificates
+RUN apk add --no-cache ca-certificates bash
 
 COPY --from=0 /tmp/rclone-v1.46-linux-amd64/rclone /usr/bin/
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
-ENTRYPOINT [ "/entrypoint.sh" ]
+ENTRYPOINT [ "/bin/bash", "-c" ]
+CMD [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,20 @@
-#!/bin/sh
-
-set -e
+#!/bin/bash
 
 URL=${WEBDAV_URL:-$PLUGIN_URL}
 USERNAME=${WEBDAV_USERNAME:-$PLUGIN_USERNAME}
 PASSWORD=${WEBDAV_PASSWORD:-$PLUGIN_PASSWORD}
-RPATH=$PLUGIN_REMOTE_PATH
+
 SOURCES=$PLUGIN_SOURCES
+
+REMOTE_PATH=${PLUGIN_REMOTE_PATH:-""}
+
+REMOTE_BASE_PATH=${PLUGIN_REMOTE_BASE_PATH:-""}
+BUILD_NUMBER_PREFIX=${PLUGIN_BUILD_NUMBER_PREFIX:-"true"}
+BUILD_NUMBER_PREFIX=$(echo "$BUILD_NUMBER_PREFIX" | tr '[:upper:]' '[:lower:]')
+BUILD_NUMBER=${PLUGIN_BUILD_NUMER:-${DRONE_BUILD_NUMBER:-""}}
+MAX_BUILD_RECORD=${PLUGIN_MAX_BUILD_RECORDE:-"10"}
+
+set -euo pipefail
 
 rclone config create \
     remote webdav \
@@ -14,13 +22,39 @@ rclone config create \
     url  $URL \
     user $USERNAME \
     > /dev/null 2>&1
-
 rclone config password remote pass $PASSWORD > /dev/null 2>&1
 
 for SPATH in $(echo "$SOURCES" | sed "s/,/ /g"); do
     if [ -d "$SPATH" ] || [ -f "$SPATH" ] ; then
-        rclone copy $SPATH remote:$RPATH/$(basename $SPATH) -v -P
+        RPATH="${REMOTE_PATH}"
+        
+        if [ -n "$REMOTE_BASE_PATH" ] ; then
+            RPATH="$REMOTE_BASE_PATH"
+            if [ "true" == "$BUILD_NUMBER_PREFIX" ]; then 
+                RPATH="${RPATH}/${BUILD_NUMBER}"
+            fi
+            RPATH="${RPATH}/$(dirname $SPATH)"
+        fi
+        
+        echo "cloning $SPATH to remote:$RPATH/$(basename $SPATH) "
+        rclone copy $SPATH remote:$RPATH/$(basename $SPATH) 
+
     else
         echo "WARNING: source path $SPATH does not exist"
+
     fi
 done
+
+
+if [ -n "$REMOTE_BASE_PATH" ] && [ "true" == "$BUILD_NUMBER_PREFIX" ] && [ "$MAX_BUILD_RECORD" -eq "$MAX_BUILD_RECORD" ]  2>/dev/null ; then
+    i=0
+    for dir in `rclone lsf --dirs-only --dir-slash=false "remote:$REMOTE_BASE_PATH" | sort -r`; do 
+        if [ "$dir" -eq "$dir" ] 2>/dev/null ; then
+            ((i=i+1))
+            if  [ "$dir" -ne "$BUILD_NUMBER" ] && [ "$i" -gt "$MAX_BUILD_RECORD" ]; then 
+                echo "removing remote:$REMOTE_BASE_PATH/$dir"
+                rclone purge "remote:$REMOTE_BASE_PATH/$dir"
+            fi
+        fi
+    done
+fi
